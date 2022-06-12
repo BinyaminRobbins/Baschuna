fd1b5a864501636ab5235e9336cc505aaf67a5f2
SyntAppsDev <syntappsdev@gmail.com>
2022-06-12 22:14:23 +0300

    first attempt core-splash lib.
    Signed-off-by: SyntAppsDev <syntappsdev@gmail.com>

--- app/src/main/java/com/syntapps/bashcuna/ui/activities/AuthActivity.kt
+++ app/src/main/java/com/syntapps/bashcuna/ui/activities/AuthActivity.kt
@@ -2,10 +2,12 @@ package com.syntapps.bashcuna.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class AuthActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
     }
 }
