a16475a25f7e69e35cad31a85f3a57738887d2f3
Binyamin Robbins <binyamin.robbins@gmail.com>
2022-06-12 20:35:33 +0300

    colors, themes.xml, and first activity, mvvm setup.
    Signed-off-by: SyntAppsDev <syntappsdev@gmail.com>

--- app/src/main/java/com/syntapps/bashcuna/ui/activities/AuthActivity.kt
+++ app/src/main/java/com/syntapps/bashcuna/ui/activities/AuthActivity.kt
@@ -1,11 +1,11 @@
-package com.syntapps.bashcuna
+package com.syntapps.bashcuna.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class AuthActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/syntapps/bashcuna/ui/viewmodels/AuthViewModel.kt
@@ -0,0 +1,4 @@
+package com.syntapps.bashcuna.ui.viewmodels
+
+class AuthViewModel {
+}
