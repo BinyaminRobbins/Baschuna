7ee7a37645fea116a8064ef6ab5b60213a9fc295
Binyamin Robbins <binyamin.robbins@gmail.com>
2022-06-13 11:54:58 +0300

    splash screen (non-animated)
    Signed-off-by: SyntAppsDev <syntappsdev@gmail.com>

--- app/src/main/java/com/syntapps/bashcuna/ui/activities/AuthActivity.kt
+++ app/src/main/java/com/syntapps/bashcuna/ui/activities/AuthActivity.kt
@@ -2,12 +2,17 @@ package com.syntapps.bashcuna.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class AuthActivity : AppCompatActivity() {
+    private var keepSplashOnScreen = true
+    private val delay = 500L
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
+        installSplashScreen().setKeepOnScreenCondition { keepSplashOnScreen }
+        Handler(Looper.getMainLooper()).postDelayed({ keepSplashOnScreen = false }, delay)
         super.onCreate(savedInstanceState)
     }
 }
