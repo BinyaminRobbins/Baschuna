9e8df2c87db81e59f974d0c6138041bde7f5f870
Binyamin Robbins <binyamin.robbins@gmail.com>
2022-07-04 19:58:13 +0300

    toggle button - gonna hook up viewpager to it.
    Signed-off-by: SyntAppsDev <syntappsdev@gmail.com>

--- app/src/main/java/com/syntapps/bashcuna/ui/fragments/home/employer/JobsFragment.kt
+++ app/src/main/java/com/syntapps/bashcuna/ui/fragments/home/employer/JobsFragment.kt
@@ -5,10 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.material.button.MaterialButtonToggleGroup
 import com.syntapps.bashcuna.R
 
 class JobsFragment : Fragment() {
 
+    private lateinit var toggleGroup: MaterialButtonToggleGroup
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +23,11 @@ class JobsFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        toggleGroup = view.findViewById(R.id.toggleGroup)
+        toggleGroup.addOnButtonCheckedListener { toggleButton, checkedId, isChecked ->
+            // Respond to button selection
+        }
     }
 
 
